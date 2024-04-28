dff597ce5c711c2c37e9352a65e9c5911b730cee
Viag <ismael.rousseau@gmail.com>
2024-04-28 21:59:18 +0000

    computing learning percentage metric

--- compute_perplexity.py
+++ compute_perplexity.py
@@ -42,13 +42,60 @@ for instruction, _input, _output in zip(dataset["train"]["instruction"], dataset
         break
 ds = Dataset.from_dict(new_data)
 ds = ds.train_test_split(test_size=0.15)
-print(ds)
-s1 = ds["train"]["prompt"][0]
-s2 = ds["train"]["prompt"][0]
-s3 = ds["train"]["prompt"][0]
 
-results = perplexity.compute(predictions=[s1, s2, s3], model_id='./phi-3-alpaca_instruct-tuned-k1000')
-print(results)
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-results = perplexity.compute(predictions=[s1, s2, s3], model_id='microsoft/Phi-3-mini-128k-instruct')
-print(results)
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    "./phi-3-alpaca_instruct-tuned-k1000",
+    device_map="auto",
+    quantization_config=nf4_config,
+    use_cache=False,
+    trust_remote_code=True,
+)
+tokenizer = AutoTokenizer.from_pretrained("./phi-3-alpaca_instruct-tuned-k1000")
+
+def calculate_perplexity(sentence, model, tokenizer):
+    # Load pre-trained model and tokenizer
+    # Encode the sentence and add batch dimension
+    inputs = tokenizer(sentence, return_tensors='pt')
+
+    # Calculate log likelihood across the sequence
+    with torch.no_grad():
+        outputs = model(**inputs, labels=inputs["input_ids"])
+        log_likelihood = outputs[0]  # This is the loss (negative log likelihood)
+    # Calculate perplexity
+    perplexity = torch.exp(log_likelihood)
+
+    return perplexity.item()
+
+perplexities_finetuned_model = []
+for sentence in ds["train"]["prompt"][:20]:
+    perplexity = calculate_perplexity(sentence, model, tokenizer)
+    perplexities_finetuned_model.append(perplexity)
+
+
+# print(f"The perplexity of the sentence is: {perplexity}")
+
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3-mini-128k-instruct",
+    device_map="auto",
+    quantization_config=nf4_config,
+    use_cache=False,
+    trust_remote_code=True,
+)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-128k-instruct")
+
+perplexities_base_model = []
+for sentence in ds["train"]["prompt"][:20]:
+    perplexity = calculate_perplexity(sentence, model, tokenizer)
+    perplexities_base_model.append(perplexity)
+
+for pp_ft, pp_bm in zip(perplexities_finetuned_model, perplexities_base_model):
+    print(100*((pp_bm-pp_ft)/pp_bm))
